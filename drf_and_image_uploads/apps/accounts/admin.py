b6af21a4b1c12f2287eb99b2d4a9a11983820178
Aidas Bendoraitis <aidasbend@yahoo.com>
2020-03-22 01:06:25 +0100

    🔨 User administration updated.

--- drf_and_image_uploads/apps/accounts/admin.py
+++ drf_and_image_uploads/apps/accounts/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as UserAdminBase
+from django.utils.translation import gettext_lazy as _
 
 from .models import User
 
 @admin.register(User)
-class UserAdmin(admin.ModelAdmin):
-    pass
+class UserAdmin(UserAdminBase):
+    readonly_fields = ["username"]
+
+    fieldsets = list(UserAdminBase.fieldsets) + [
+        (_("Extra"), {"fields": ["bio", "avatar"]})
+    ]
